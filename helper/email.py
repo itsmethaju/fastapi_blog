f7ae128d8111d12d82380fa5fdc454a933a80d66
itsmethaju <wondringpsycho@gmail.com>
2024-06-06 01:55:02 +0530

    added_dotenv

--- helper/email.py
+++ helper/email.py
@@ -1,14 +1,17 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Function to send confirmation email
 def send_confirmation_email(email):
     # Set up SMTP connection
     smtp_host = 'smtp.gmail.com'  # Update with your SMTP server
     smtp_port = 587  # Update with your SMTP port
-    smtp_username = ''
-    smtp_password = ''
+    smtp_username = os.getenv("smtp_username")
+    smtp_password = os.getenv("smtp_password")
 
 
     server = smtplib.SMTP(smtp_host, smtp_port)
